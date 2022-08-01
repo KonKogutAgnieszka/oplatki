3db89a5368d5e5c9889e4c7b5332f9f30db48f84
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2022-08-01 18:21:34 +0200

    feat: gallery animation

--- script/gallery.js
+++ script/gallery.js
@@ -2,12 +2,14 @@ const photos = ['../img/gallery/1.png',
 '../img/gallery/2.png',
 '../img/gallery/3.png',
 '../img/gallery/4.png',
+'../img/gallery/5.png',
 ]
 
 const authors = ['Irena Luszczyk',
 'Ewelina Puchalska',
 'Anna Kopiec',
 'Natalia Kowalska',
+'Sylwia Regulska'
 ]
 
 const photoDiv = document.querySelector('div.photo');
